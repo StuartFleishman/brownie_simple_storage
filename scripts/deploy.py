30744758542633ec6a3118aaeb6f6ba2d7d17a0b
StuartFleishman <Stuart.Alex.Fleishman@gmail.com>
2022-01-17 16:59:40 -0500

    running tests

--- scripts/deploy.py
+++ scripts/deploy.py
@@ -6,7 +6,10 @@ def deploy_simple_storage():
   # print(account)
   simple_storage = SimpleStorage.deploy({"from": account})
   stored_value = simple_storage.retrieve()
-  print(stored_value)
+  transaction = simple_storage.store(15, {"from": account})
+  transaction.wait(1)
+  updated_stored_value = simple_storage.retrieve()
+  print(updated_stored_value)
 
 def main(): 
     deploy_simple_storage() 
