a95e1c96a7ff3d35dc26a3671fa5987d427d7114
StuartFleishman <Stuart.Alex.Fleishman@gmail.com>
2022-01-19 09:56:01 -0500

    read value from contarct

--- scripts/deploy.py
+++ scripts/deploy.py
@@ -1,7 +1,7 @@
-from brownie import accounts, SimpleStorage
+from brownie import accounts, SimpleStorage, network
 
 def deploy_simple_storage():
-  account = accounts[0]
+  account = get_account()
   # account = accounts.load("stu-account")
   # print(account)
   simple_storage = SimpleStorage.deploy({"from": account})
@@ -10,6 +10,12 @@ def deploy_simple_storage():
   transaction.wait(1)
   updated_stored_value = simple_storage.retrieve()
   print(updated_stored_value)
+
+def get_account():
+    if network.show_active() == "development":
+        return accounts[0]
+    else:
+      return accounts.load("stu-account")
 
 def main(): 
     deploy_simple_storage() 
